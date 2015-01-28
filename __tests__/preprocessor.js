7e5cd3587f28c13059fca3dbf3d96919d2fba894
Clément Dubois <cdubois@octo.com>
2015-01-28 18:30:14 +0100

    add test for selected list

--- __tests__/preprocessor.js
+++ __tests__/preprocessor.js
@@ -1,7 +1,7 @@
 var ReactTools = require('react-tools');
 var MAGIC = "/** @jsx ";
 module.exports = {
-  process: function(src, file) {
+  process: function(src) {
     if (src.slice(0, MAGIC.length) != MAGIC) return src;
     return ReactTools.transform(src);
   }
--- __tests__/react-multiple-select-test.js
+++ __tests__/react-multiple-select-test.js
@@ -3,25 +3,48 @@
 // tests/CheckboxWithLabel-test.js
 
 jest.dontMock('../lib/react-multiple-select.js');
+jest.dontMock('lodash');
+
 describe('MultipleSelect', function() {
 
-  it('Shows the options', function() {
-    var React = require('react/addons');
-    var MultipleSelect = require('../lib/react-multiple-select.js');
-    var TestUtils = React.addons.TestUtils;
+  var React = require('react/addons');
+  var MultipleSelect = require('../lib/react-multiple-select.js');
+  var TestUtils = React.addons.TestUtils;
 
-    // Render a checkbox with label in the document
+  it('Should show the good options values and texts', function() {
+    // Given
     var list = [{id:1, content:'one'},{id:2, content:'two'}];
     var select = TestUtils.renderIntoDocument(
       <MultipleSelect list={list} selected={[]}/>
     );
 
-    // Verify that it's Off by default
+    // When
     var options = TestUtils.scryRenderedDOMComponentsWithTag(
       select, 'option');
+
+    // Then
     expect(options.length).toBe(2);
     expect(options[0].getDOMNode().textContent).toEqual('one');
+    expect(options[0].getDOMNode().value).toEqual('1');
     expect(options[1].getDOMNode().textContent).toEqual('two');
+    expect(options[1].getDOMNode().value).toEqual('2');
+  });
+
+  it('Should select the selected list', function(){
+    // Given
+    var list = [{id:3, content:'three'},{id:4, content:'four'}];
+    var selected = [{id:4, content:'four'}];
+    var select = TestUtils.renderIntoDocument(
+      <MultipleSelect list={list} selected={selected}/>
+    );
+
+    //When
+    var options = TestUtils.scryRenderedDOMComponentsWithTag(
+      select, 'option');
+
+    //Then
+    expect(options[0].getDOMNode().selected).toBeFalsy();
+    expect(options[1].getDOMNode().selected).toBeTruthy();
   });
 
 });
